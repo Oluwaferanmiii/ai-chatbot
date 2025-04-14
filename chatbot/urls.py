140dfbb436cb429ce77a9a398c356d8521cca86a
Feranmi Akinwale <feranmiphileo@gmail.com>
2025-04-14 12:05:16 +0200

    debug

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('', views.login_user, name='root'),
 ]
