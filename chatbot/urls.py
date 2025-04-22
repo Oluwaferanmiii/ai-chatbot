35c5fc06fb3f7d451165142eca49ce52f3553cad
Feranmi Akinwale <feranmiphileo@gmail.com>
2025-04-22 23:30:38 +0200

    Fixed dashboard and new chat

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('new-chat/', views.new_chat, name='new_chat'),
+    path('send-message/', views.send_message, name='send_message'),
     path('', views.login_user, name='root'),
 ]
